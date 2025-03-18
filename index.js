7401aa03b9f5ae7e581943ad232f2e8986266986
Ahmed Raza <bhaia9036@gmail.com>
2025-03-19 01:50:01 +0500

    Updann  asdasasdaas aw

--- index.js
+++ index.js
@@ -62,14 +62,16 @@ app.post("/login", (req, res) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: true,
+        path: '/',
         // sameSite: "none",
         maxAge: 15 * 60 * 1000, // 15 minutes
     });
- 
+
     // Set Refresh Token Cookie
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: true,
+        path: '/',
         // sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
