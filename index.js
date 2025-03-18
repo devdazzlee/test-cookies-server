105beed423ba83f2988f9c8367d641f44ddaae17
Ahmed Raza <bhaia9036@gmail.com>
2025-03-19 01:45:43 +0500

    Updann  asdas

--- index.js
+++ index.js
@@ -61,16 +61,16 @@ app.post("/login", (req, res) => {
     // Set Access Token Cookie
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // Secure only in production
-        sameSite: "none",
+        secure: true,
+        // sameSite: "none",
         maxAge: 15 * 60 * 1000, // 15 minutes
     });
 
     // Set Refresh Token Cookie
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "none",
+        secure: true,
+        // sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
