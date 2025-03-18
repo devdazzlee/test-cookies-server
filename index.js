e25af20440df2ffe0af324eb3daef69e6dabbd37
Ahmed Raza <bhaia9036@gmail.com>
2025-03-19 04:07:56 +0500

    asdas

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.set('trust proxy', 1);
 
 
 // Middlewares
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
 app.use(
     cors({
         origin: "http://localhost:3000", // Adjust for frontend
